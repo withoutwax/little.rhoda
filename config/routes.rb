b0ecfccdba6fe6ef8eb57531f9e9bbb7717e6dd9
Will Kim <rlagmlckd@gmail.com>
2018-03-18 14:02:31 -0500

    Created Index, Products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
+  resources :products
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

f27e150fd4f17995f2445360e0182d02cd3d0f39
Will Kim <rlagmlckd@gmail.com>
2018-03-19 02:10:23 -0500

    Implemented basic CRUD (2/2)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,18 +3,43 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
   end
 
   def create
     @product = Product.new(product_params)
 
-    @product.save
-    redirect_to @product
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
   end
 
   private

d96edec6a6286f618ee76a1dfe21875f806f39ee
Will Kim <rlagmlckd@gmail.com>
2018-03-19 01:49:00 -0500

    Created basic CRUD (1/2)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,24 @@
 class ProductsController < ApplicationController
-  def new
-
+  def index
+    @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:title, :description, :price)
+    end
 end
